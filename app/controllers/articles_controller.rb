a4183bb9330f40d07b671bac7e47c8f6bf994027
Angel Melendez <iquit@osx-iquit.localdomain>
2018-06-11 13:46:28 -0400

    hello Angel

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,14 @@
 class ArticlesController < ApplicationController
-  def new
+    def new
+    end
+
+    def create
+      @article = Article.new(title: params["article"]["title"], text: params["article"]["text"])
+      @article.save
+      redirect_to @article
+    end
+    def show
+      # render json: params
+      @article = Article.find params[:id]
+    end
 end
